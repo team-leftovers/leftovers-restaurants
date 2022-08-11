f512a4b5e4ca3f55741a4e6b98f093f6ff6187c3
gjmistkawi <gjmistkawi@gmail.com>
2022-08-11 08:26:38 -0700

    left-45: restaurant details

--- src/main/java/com/leftovers/restaurants/controller/RestaurantController.java
+++ src/main/java/com/leftovers/restaurants/controller/RestaurantController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.Time;
+import java.util.List;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping(path = "/restaurants")
@@ -42,6 +44,31 @@ public class RestaurantController {
         return new ResponseEntity<Restaurant>(HttpStatus.OK);
     }
 
+    // Restaurant Getters
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public ResponseEntity<Iterable<Restaurant>> getAllRestaurants() {
+        var result = restaurants.getAllRestaurants();
+        return new ResponseEntity<Iterable<Restaurant>>(result, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "{id}", method = RequestMethod.GET)
+    public ResponseEntity<Restaurant> getRestaurantById(@PathVariable Integer id) {
+        var result = restaurants.getRestaurant(id);
+        if(result == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "", params = "name", method = RequestMethod.GET)
+    public ResponseEntity<Iterable<Restaurant>> getRestaurantsByName(@RequestParam(value = "name") String name) {
+        var result = restaurants.getRestaurantByNameContaining(name);
+        if(result == null || StreamSupport.stream(result.spliterator(), false).findAny().isEmpty())
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+
+    // Validations
     boolean restaurantInputValidation(Restaurant r) {
         // Check if id exists
         if(r.getId() != null) {
--- src/main/java/com/leftovers/restaurants/dao/RestaurantDao.java
+++ src/main/java/com/leftovers/restaurants/dao/RestaurantDao.java
@@ -4,6 +4,8 @@ import com.leftovers.restaurants.model.Restaurant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class RestaurantDao {
     @Autowired
@@ -27,8 +29,13 @@ public class RestaurantDao {
     public Restaurant getRestaurant(Integer id) {
         return repo.findRestaurantById(id);
     }
-
     public Restaurant getLatestRestaurant() {
         return repo.findTopByOrderByIdDesc();
+    }
+    public Iterable<Restaurant> getAllRestaurants() {
+        return repo.findAll();
+    }
+    public Iterable<Restaurant> getRestaurantByNameContaining(String str) {
+        return repo.findRestaurantByNameContainingIgnoreCase(str);
     }
 }
