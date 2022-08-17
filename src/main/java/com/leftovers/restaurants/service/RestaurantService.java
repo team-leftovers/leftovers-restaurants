90afa320b5787b1b69bd9d4980d8e6477c1c3d04
gjmistkawi <gjmistkawi@gmail.com>
2022-08-17 00:15:36 -0700

    Service layer interfaces added

---
+++ src/main/java/com/leftovers/restaurants/service/RestaurantService.java
@@ -0,0 +1,19 @@
+package com.leftovers.restaurants.service;
+
+import com.leftovers.restaurants.dto.CreateFoodDto;
+import com.leftovers.restaurants.dto.CreateRestaurantDto;
+import com.leftovers.restaurants.dto.UpdateRestaurantDto;
+import com.leftovers.restaurants.model.Food;
+import com.leftovers.restaurants.model.Restaurant;
+
+import java.util.List;
+
+public interface RestaurantService {
+    Restaurant createNewRestaurant(CreateRestaurantDto dto);
+    List<Restaurant> getAllRestaurants();
+    Restaurant getRestaurant(Integer id);
+    Restaurant updateRestaurant(Integer id, UpdateRestaurantDto dto);
+    void deleteRestaurant(Integer id);
+    Food addFoodToRestaurant(Integer id, CreateFoodDto dto);
+    List<Food> getAllFoodByRestaurant(Integer id);
+}
