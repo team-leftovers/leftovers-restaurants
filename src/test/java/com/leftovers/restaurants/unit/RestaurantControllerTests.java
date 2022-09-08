3abe26e060d7aca020790ec731681791e9d1451e
gjmistkawi <gjmistkawi@gmail.com>
2022-09-08 09:08:08 -0700

    Updated Restaurant Controller testing - Food Controller test imports removed

--- src/test/java/com/leftovers/restaurants/unit/RestaurantControllerTests.java
+++ src/test/java/com/leftovers/restaurants/unit/RestaurantControllerTests.java
@@ -15,7 +15,6 @@ import com.leftovers.restaurants.repository.TagRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
